b39a247caef4902ad8772922554a3652738f7f8d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2019-01-26 16:47:29 +0900

    libgit2 の関数を呼び出せるようにする

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <git2.h>
 
 int main()
 {
+	git_repository *repo = NULL;
+	int ret;
+	
+	git_libgit2_init();
+
+	ret = git_repository_open_ext(&repo, ".", 0, NULL);
+	printf("git_repository_open_ext() returns %d\n", ret);
+
+	git_repository_free(repo);
+	git_libgit2_shutdown();
 	return 0;
 }
